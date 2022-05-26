5ec89b9c69460823da7e99086d073be53b3dd0f8
Artiprocher <wangye87v5@hotmail.com>
2022-05-26 20:16:10 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="TriangleStrategy",
+    version="1.0.0",
+    author="Artiprocher",
+    author_email="zjduan@stu.ecnu.edu.cn",
+    description="TriangleStrategy is a high-efficiency reinforcement learning based algorithmic trading library.",
+    url="https://github.com/ECNU-CILAB/TriangleStrategy", 
+    packages=[]
+)
