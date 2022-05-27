bfd90fb92ed4c093aeef1dec230f8974c41ef981
Artiprocher <wangye87v5@hotmail.com>
2022-05-27 11:41:04 +0800

    ...

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="TriangleStrategy",
@@ -7,5 +7,5 @@ setup(
     author_email="zjduan@stu.ecnu.edu.cn",
     description="TriangleStrategy is a high-efficiency reinforcement learning based algorithmic trading library.",
     url="https://github.com/ECNU-CILAB/TriangleStrategy", 
-    packages=find_packages()
+    packages=["torch", "numpy"]
 )
