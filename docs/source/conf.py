1b67aad6329612e930d27a00a7901ab59d75771c
Artiprocher <wangye87v5@hotmail.com>
2022-05-27 12:07:59 +0800

    ...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,6 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
-    "sphinxcontrib.bibtex",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
