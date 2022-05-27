eab0d06e8e643cccb2e234b8bc3bc740d94b4829
Artiprocher <wangye87v5@hotmail.com>
2022-05-27 12:21:30 +0800

    ...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
-print("path:", os.path.abspath('../'))
+print("path:", os.path.abspath('../../'))
+print("file list:", os.listdir(os.path.abspath('../../')))
 
 # -- Project information -----------------------------------------------------
 
